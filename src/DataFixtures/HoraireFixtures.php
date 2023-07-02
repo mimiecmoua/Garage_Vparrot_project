88fc1fa10b359c3b581473ac8e1a2bfa13c52936
Emilie <dionatsumi@gmail.com>
2023-07-02 18:44:53 +0200

    Add OccasionFixtures

--- src/DataFixtures/HoraireFixtures.php
+++ src/DataFixtures/HoraireFixtures.php
@@ -12,10 +12,10 @@ class HoraireFixtures extends Fixture
     {
         $lundi = new Horaire();
         $lundi->setJour('Lundi');
-        $lundi->setDebutAm('09h30');
-        $lundi->setFinAm('12h30');
+        $lundi->setDebutAm('08h45');
+        $lundi->setFinAm('12h00');
         $lundi->setDebutPm('14h00');
-        $lundi->setFinPm('19h00');
+        $lundi->setFinPm('18h00');
         $lundi->setFermeture(false);
         $lundi->setFermeture2(false);
 
@@ -23,30 +23,30 @@ class HoraireFixtures extends Fixture
 
         $mardi = new Horaire();
         $mardi->setJour('Mardi');
-        $mardi->setDebutAm('09h30');
-        $mardi->setFinAm('12h30');
+        $mardi->setDebutAm('08h45');
+        $mardi->setFinAm('12h00');
         $mardi->setDebutPm('14h00');
-        $mardi->setFinPm('19h00');
+        $mardi->setFinPm('18h00');
         $mardi->setFermeture(false);
         $mardi->setFermeture2(false);
         $manager->persist($mardi);
 
         $mercredi = new Horaire();
         $mercredi->setJour('Mercredi');
-        $mercredi->setDebutAm('09h30');
-        $mercredi->setFinAm('12h30');
+        $mercredi->setDebutAm('08h45');
+        $mercredi->setFinAm('12h00');
         $mercredi->setDebutPm('14h00');
-        $mercredi->setFinPm('19h00');
+        $mercredi->setFinPm('18h00');
         $mercredi->setFermeture(false);
         $mercredi->setFermeture2(false);
         $manager->persist($mercredi);
 
         $jeudi = new Horaire();
         $jeudi->setJour('Jeudi');
-        $jeudi->setDebutAm('09h30');
-        $jeudi->setFinAm('12h30');
+        $jeudi->setDebutAm('08h45');
+        $jeudi->setFinAm('12h00');
         $jeudi->setDebutPm('14h00');
-        $jeudi->setFinPm('19h00');
+        $jeudi->setFinPm('18h00');
         $jeudi->setFermeture(false);
         $jeudi->setFermeture2(false);
         $manager->persist($jeudi);
@@ -54,18 +54,18 @@ class HoraireFixtures extends Fixture
 
         $vendredi = new Horaire();
         $vendredi->setJour('Vendredi');
-        $vendredi->setDebutAm('09h30');
-        $vendredi->setFinAm('12h30');
+        $vendredi->setDebutAm('08h45');
+        $vendredi->setFinAm('12h00');
         $vendredi->setDebutPm('14h00');
-        $vendredi->setFinPm('19h00');
+        $vendredi->setFinPm('18h00');
         $vendredi->setFermeture(false);
         $vendredi->setFermeture2(false);
         $manager->persist($vendredi);
 
         $samedi = new Horaire();
         $samedi->setJour('Samedi');
-        $samedi->setDebutAm('09h30');
-        $samedi->setFinAm('13h00');
+        $samedi->setDebutAm('08h45');
+        $samedi->setFinAm('12h00');
         $samedi->setDebutPm('');
         $samedi->setFinPm('');
         $samedi->setFermeture(false);
